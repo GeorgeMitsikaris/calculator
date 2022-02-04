a2938afd128f6d212368af1977d58d2ac8cdb50a
George Mitsikaris <george.mitsikaris@gmail.com>
2022-02-04 21:50:35 +0200

    Added scss functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div className="App">
-     
+      <Button />
     </div>
   );
-}
+} 
 
 export default App;
